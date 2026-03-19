3256547dca2e0ede81eb3cd9552cc510d53374cc
Adarsh22 <adarshthakurwork620@gmail.com>
2026-03-19 11:34:15 +0530

    Delete Conditionals/Question_1.py

--- Conditionals/Question_1.py
+++
@@ -1,9 +0,0 @@
-'''Take a number and check if it is positive, negative, or zero.'''
-
-a=int(input("Enter number:"))
-if a<0:
-    print("Negative")
-elif a==0:
-    print("Zero")
-else:
-    print("positive")        
