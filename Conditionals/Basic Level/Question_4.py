8b59740267d6fd62fd0097edd22361af1394720f
Adarsh22 <adarshthakurwork620@gmail.com>
2026-03-19 11:33:48 +0530

    Add files via upload

---
+++ Conditionals/Basic Level/Question_4.py
@@ -0,0 +1,9 @@
+'''Take a character and check if it is a vowel or consonant.'''
+
+char=str(input("Enter character(a-z):"))
+if char=="a" or char=="e" or char=="i"or char=="o"or char=="u":
+    print("Vowel")
+elif char=="A" or char=="E" or char=="I"or char=="O"or char=="U":
+    print("Vowel")
+else:
+    print("Consonant")     
