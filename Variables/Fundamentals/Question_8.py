0fd7875df8ba94c80e69f94d76295a663808673b
Adarsh22 <adarshthakurwork620@gmail.com>
2026-03-23 08:44:52 +0530

    Add files via upload

---
+++ Variables/Fundamentals/Question_8.py
@@ -0,0 +1,17 @@
+'''10. Create three variables:
+    
+    length = 5
+    
+    width = 2.5
+    
+    Triangle area and print result. Also print its data type.'''
+
+
+length = 5
+    
+width = 2.5
+
+area= length*width
+
+print("Area=",area)
+print("its data type=",type(area))
