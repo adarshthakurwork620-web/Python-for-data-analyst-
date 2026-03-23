91967bab95491282ded3c4264d909efc2e852542
Adarsh22 <adarshthakurwork620@gmail.com>
2026-03-23 08:51:16 +0530

    Add files via upload

---
+++ Operators/ARITHMETIC OPERATORS/Question_3.py
@@ -0,0 +1,10 @@
+'''Take price and discount %, calculate final price.'''
+
+price= float(input("Price:"))
+
+discount= price*(18/100)
+
+final_price=price+discount
+
+print("discount=",discount)
+print("final price=",final_price)
