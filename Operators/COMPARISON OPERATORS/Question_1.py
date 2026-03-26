625119aed8b686b231a82aaf5b90e1236745d313
Adarsh22 <adarshthakurwork620@gmail.com>
2026-03-26 08:09:32 +0530

    Add files via upload

---
+++ Operators/COMPARISON OPERATORS/Question_1.py
@@ -0,0 +1,9 @@
+'''Take two integers and check if they are equal.'''
+
+a=int(input("1st number:"))
+b=int(input("2nd number:"))
+
+if a==b:
+    print("they are equal")
+else:
+    print("they are not equal ")    
