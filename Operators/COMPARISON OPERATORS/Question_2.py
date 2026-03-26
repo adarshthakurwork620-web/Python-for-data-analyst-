67b13238c336d10fa54800f2dee92ee4653d39a1
Adarsh22 <adarshthakurwork620@gmail.com>
2026-03-26 08:09:58 +0530

    Add files via upload

---
+++ Operators/COMPARISON OPERATORS/Question_2.py
@@ -0,0 +1,12 @@
+'''Take password input and check if it equals "admin".
+ Take username and check if it equals "Sagar".'''
+
+username=str(input("Username:"))
+passward=str(input("Passward:"))
+
+print("")
+if username=="sager":
+    print("Correct username")
+print("")        
+if passward=="admin":
+    print("Correct passward")  
