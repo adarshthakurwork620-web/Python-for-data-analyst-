930121ebddde507f3ff1c09d09b0fd5e55fb9c9d
Adarsh22 <adarshthakurwork620@gmail.com>
2026-03-27 09:49:45 +0530

    Add files via upload

---
+++ Operators/LOGICAL OPERATORS/Question_1.py
@@ -0,0 +1,9 @@
+'''Take two number inputs and check if both are same and greater than 10.'''
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+
+if a==b and a+b>10:
+    print("varified the question")
+else:
+    print("wrong input!! try again")    
