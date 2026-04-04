b4d1118cc26bc0b150f10b89198cc3b62825536e
Adarsh22 <adarshthakurwork620@gmail.com>
2026-04-04 10:00:06 +0530

    Add files via upload

---
+++ List-Tuple/EASY LEVEL ()/Question_5.py
@@ -0,0 +1,16 @@
+'''Return list with duplicates removed.'''
+ 
+n=int(input("Enter the size of list:"))
+
+list=[]
+for i in range(1,n+1):
+    num=int(input(f"Enter number {i} :"))
+    list.append(num)
+print(list)    
+ 
+unique=0
+for i in range(len(list)):
+    if i not in list:
+        uniqueadd
+print(unique)       
+
