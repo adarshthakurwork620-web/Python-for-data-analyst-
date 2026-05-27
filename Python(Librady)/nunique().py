b3b5096509bc9e5110edeaedc15d1765b8f45853
Adarsh22 <adarshthakurwork620@gmail.com>
2026-05-27 10:41:40 +0530

    Delete Python(Librady)/nunique().py

--- Python(Librady)/nunique().py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-df=pd.read_excel("pandas/combined_data.xlsx")
-print(df)
-
-print(df["Resturant_ID"].nunique())
-print(df["deliver_status"].nunique())
-print(df["quantity"].nunique())
-print(df["payment_method"].nunique())
