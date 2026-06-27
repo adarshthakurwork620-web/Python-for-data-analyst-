2f64addf18515d68062008deb647a9228fdb9c2c
Adarsh22 <adarshthakurwork620@gmail.com>
2026-06-27 22:37:11 +0530

    Add files via upload

--- Python(Librady) /Matplotlib/foundation of all charts/Line_Chart.py
+++ Python(Librady) /Matplotlib/foundation of all charts/Line_Chart.py
@@ -1,10 +1,23 @@
+"""
+What is a Line Chart?
+
+A line chart is used to show trends or changes in data over time. It connects data points with lines, making it easy to understand whether values are increasing, decreasing, or fluctuating.
+
+"""
+
+'''
+When do you use Line Chart?
+
+I use a line chart when the data is ordered or time-based, such as monthly sales, yearly revenue, daily temperature, or website traffic.
+'''
+
 import pandas as pd
 import matplotlib.pyplot as plt
 #pyplot = plotting module
 
 Data ={
-         "months":["Jan", "Feb", "Mar", "Apr"],
-         "sales" :[100, 150, 120, 180],
+         "months":["Jan","Feb","Mar","Apr","May","Jun"],
+         "sales" :[120,180,160,220,250,240],
       }
 
 df = pd.DataFrame(Data)
@@ -15,5 +28,22 @@ plt.xlabel("months")
 # # Y Label
 plt.ylabel("sales")
 
-plt.plot(df["months"],df["sales"])
+plt.plot(df["months"],df["sales"], marker="o")
 plt.show()
+
+'''
+3 insights:-
+
+1. Highest sales month?--->May
+2. Lowest sales month?--->Jan
+3. Overall trend increasing or fluctuating? 
+
+---> fluctuating but mostly increasing
+Jan 120 → Feb 180 ↑
+Feb 180 → Mar 160 ↓
+Mar 160 → Apr 220 ↑
+Apr 220 → May 250 ↑
+May 250 → Jun 240 ↓
+
+'''
+
